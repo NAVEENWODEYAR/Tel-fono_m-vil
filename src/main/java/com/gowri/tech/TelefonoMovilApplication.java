5fff89fb70ad0b738561f3707d0e494a7448441b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-01 13:10:39 +0530

    initial commit

---
+++ src/main/java/com/gowri/tech/TelefonoMovilApplication.java
@@ -0,0 +1,13 @@
+package com.gowri.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonoMovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonoMovilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowri/tech/TelefonoMovilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelefonoMovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
