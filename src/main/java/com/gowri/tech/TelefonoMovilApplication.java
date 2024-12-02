911a290204add1e7048f8af701babf5e209cfc2c
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-02 22:44:59 +0530

    db propeties added

--- src/main/java/com/gowri/tech/TelefonoMovilApplication.java
+++ src/main/java/com/gowri/tech/TelefonoMovilApplication.java
@@ -1,15 +1,17 @@
 package com.gowri.tech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelefonoMovilApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TelefonoMovilApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TelefonoMovilApplication.class, args);
-		System.out.println("Java Version: " + System.getProperty("java.version"));
+		log.info("####\nJava Version: " + System.getProperty("java.version"));
 
 	}
-
 }
