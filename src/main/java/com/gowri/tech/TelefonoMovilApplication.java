94d4ac8c01478b2aa3609670835766593ba73d27
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-02 16:33:58 +0530

    db config need to added

--- src/main/java/com/gowri/tech/TelefonoMovilApplication.java
+++ src/main/java/com/gowri/tech/TelefonoMovilApplication.java
@@ -8,6 +8,8 @@ public class TelefonoMovilApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelefonoMovilApplication.class, args);
+		System.out.println("Java Version: " + System.getProperty("java.version"));
+
 	}
 
 }
