842532f19a6d4c8bc4f899d12002d58b819d83cb
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-13 20:31:38 +0530

    Prime Numbers

---
+++ src/main/java/com/gowri/tech/string/PrimeNumbers.java
@@ -0,0 +1,31 @@
+package com.gowri.tech.string;/*
+ * @author NaveenWodeyar
+ * @date 13-12-2024
+ */
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int limit = 100; // You can set any limit here
+        System.out.println("Prime numbers up to " + limit + ":");
+
+        for (int num = 2; num <= limit; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
