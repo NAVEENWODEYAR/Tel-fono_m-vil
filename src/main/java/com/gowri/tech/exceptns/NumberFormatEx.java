292787fa2916a802554e5b945ea008904ac02cf8
Naveen <naveenkrishnawodeyar@gmail.com>
2025-01-20 23:52:22 +0530

    NumberFormatException Example

---
+++ src/main/java/com/gowri/tech/exceptns/NumberFormatEx.java
@@ -0,0 +1,20 @@
+package com.gowri.tech.exceptns;
+/*
+ * @author NaveenWodeyar
+ * @date 20-01-2025
+ */
+
+public class NumberFormatEx {
+
+    public static void numberFormatExcptn(String st){
+        Integer i = Integer.parseInt(st);
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        String arg = args[0];
+        Integer i = Integer.parseInt(arg);
+        numberFormatExcptn(String.valueOf(i));
+        System.out.println(i);
+    }
+}
