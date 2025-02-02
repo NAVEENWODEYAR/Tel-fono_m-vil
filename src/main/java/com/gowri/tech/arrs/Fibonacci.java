baebedab016b5b0a202fa74295668e2ba7a43044
Naveen <naveenkrishnawodeyar@gmail.com>
2025-02-02 19:05:52 +0530

    Fibonacci

---
+++ src/main/java/com/gowri/tech/arrs/Fibonacci.java
@@ -0,0 +1,45 @@
+package com.gowri.tech.arrs;/*
+ * @author NaveenWodeyar
+ * @date 02-02-2025
+ */
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        // Scanner to take user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for the number of terms in the Fibonacci series
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        // Print Fibonacci series
+        System.out.print("Fibonacci Series: ");
+
+        // First two Fibonacci numbers
+        int first = 0, second = 1;
+
+        // Handle the case for 0 or 1 terms
+        if (n > 0) {
+            System.out.print(first + " ");
+        }
+        if (n > 1) {
+            System.out.print(second + " ");
+        }
+
+        // Generate Fibonacci numbers from the 3rd term
+        for (int i = 3; i <= n; i++) {
+            int next = first + second;
+            System.out.print(next + " ");
+
+            // Update the previous two numbers
+            first = second;
+            second = next;
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
+
